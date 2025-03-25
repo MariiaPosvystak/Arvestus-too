e8d7bb56b76d4202cdf4752638b036150216a542
ariia Posvystak <posvystakmariia@gmail.com>
2025-03-25 22:55:16 +0200

    aFDFv

--- Arvestus_töö.py
+++ Arvestus_töö.py
@@ -16,8 +16,29 @@ print(kool(opilased, puudumised))
 
 # После заполнения списков появляется меню с выбором действий
 # Меню с выбором действий
-print("1. Узнать n лучших учеников")
-print("2. Упорядочить список в порядке возрастания прогулов. отобразить прогулы вместе с фамилиями учеников")
-print("3. Вывести список учеников отправленных на комиссию ( критерий придумать самостоятельно)")
-print("4. Отчислить(удалить из списка)  учеников, у которых прогулов больше чем 100")
-print("5. Свой вариант")
+print("\nMennu")
+print("1. Leia välja n parimat õpilast")
+print("2. Korraldage nimekiri puudumiste kasvavas järjekorras. kuvage puudumised koos õpilaste nimedega")
+print("3. Prindi komisjonile saadetud õpilaste nimekiri („Koosta oma kriteeriumid")
+print("4. Kandke välja (eemaldage nimekirjast) õpilased, kellel on üle 100 puudumise")
+print("5. Teie enda variant")
+print("6. Väljumine")
+while True:
+    try:
+        valik=int(input("Valige tegevus: "))
+    except:
+        print("Vali tegevus numbritega 1-6")
+    if valik==1:
+        print(top_n(opilased))
+    elif valik==2:
+        print(sort(opilased, puudumised))
+    elif valik==3:
+        print(commission(opilased, puudumised))
+    elif valik==4:
+        print(exclude(opilased, puudumised))
+    elif valik==5:
+        
+    elif valik==6:
+        break
+    else:
+        print("Vali tegevus numbritega 1-6")
--- module1.py
+++ module1.py
@@ -11,7 +11,10 @@ opilased=[]
 puudumised=[]
 # Заполнение списков
 def kool(opilane: str, puudub:int)->any:
-    """
+    """ Järjestab õpilased ja nende puudumised kaheks eraldi listiks
+    :param opilane: õpilase nimi
+    :param puudub: õpilase puudumise arv
+    :return: õpilased ja nende puud
     """
     while True:
         opilane=str(input("Введите имя ученика "))
@@ -24,15 +27,44 @@ def kool(opilane: str, puudub:int)->any:
         
 
 # Узнать n лучших учеников
-
+def top_n(n: int)->any:
+    """ Välistab n parimat õpilast, kellel on kõige vähem puudumisi
+    :param n: õpilaste arv
+    :return: n parimat õpilast
+    """
+    n=int(input("Введите количество лучших учеников которое вы хотите увидеть"))
+    sort_opilased=sorted(zip(opilased, puudumised))
+    k=sort_opilased[:n]
+    return k
 
 # Упорядочить список в порядке возрастания прогулов. отобразить прогулы вместе с фамилиями учеников;
-
+def sort()->any:
+    """ Sorteerib õpilased ja nende puudumised vastavalt puudumiste arvule
+    :return: sorteeritud õpilased ja nende puudumised
+    """
+    sort_puudumised=sorted(zip(opilased, puudumised))
+    return sort_puudumised
 
 # Вывести список учеников отправленных на комиссию ( критерий придумать самостоятельно)
-
+def commission()->any:
+    """ Õpilased, kellel on puudumised rohkem kui 50, saadetakse komisjoni
+    :return: õpilased ja nende puudumised, kes saadetakse komisjoni
+    """
+    l=zip(opilased, puudumised)
+    for opilased, puudumised in l:
+        if puudumised>50: 
+            return opilased, puudumised
 
 # Отчислить(удалить из списка)  учеников, у которых прогулов больше чем 100
-
+def exclude()->any:
+    """ Õpilased, kellel on puudumised rohkem kui 100, eemaldatakse
+    :return: õpilased ja nende puudumised, kes eemaldatakse
+    """
+    l=zip(opilased, puudumised)
+    for opilased, puudumised in l:
+        if puudumised>100:
+            opilased.remove()
+            puudumised.remove()
+            return opilased, puudumised
 
 # Свой вариант
